e6718cff7bdfc24cf845aa5f99476b252e156739
imekachi <imekachi@gmail.com>
2017-11-13 13:49:39 +0700

    eliminate no-used-var error

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { injectGlobal } from 'styled-components'
-
-// import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import pathChecker from './PathChecker'
-import { deadScenes, deadScenesManual } from './DeadScenes'
+// import { deadScenes, deadScenesManual } from './DeadScenes'
 // import vnConfig from './constants/vnConfig'
 import vnConfig_old from './constants/vnConfig_old'
+import pathChecker from './PathChecker'
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker'
 
 // eslint-disable-next-line no-unused-expressions
 injectGlobal`
@@ -24,17 +23,19 @@ injectGlobal`
 `
 
 // ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div style={{ padding: '30px' }}>
+      OPEN CONSOLE.
+      <br /> if no error/warning, then it should be fine.
+      <br /> if there is error, number in xxx(yyy)
+      <br /> xxx : scene id in database
+      <br /> yyy : scene id that user see (sorted scenes)
+  </div>
+  , document.getElementById('root'))
 console.log(pathChecker(null, vnConfig_old))
 // console.log(pathChecker())
 
 // console.log('>> deadScenes(): ', deadScenes(vnConfig_old))
 // console.log('>> deadScenesManual(): ', deadScenesManual(vnConfig_old))
 
-document.write(`OPEN CONSOLE. 
-if no error/warning, then it should be fine.
-if there is error, number in xxx(yyy)
-xxx : scene id in database
-yyy : scene id that user see (sorted scenes)
-`)
-
-registerServiceWorker();
+registerServiceWorker()
